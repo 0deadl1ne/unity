b9466f863974e1d6894c3ac65d917b9f13726d0a
0deadl1ne <catnaphanya48@gmail.com>
2021-04-01 20:59:26 +0300

    rpg

---
+++ RPG/Assets/Scripts/NPCCreature.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCCreature : Interectable
+{
+    protected override void Interact()
+    {
+        base.Interact();
+        Debug.Log("I have a good story for you!");
+        
+    }
+}
